254f03ccb9422d43f6daf64fb6a0c40ce699839b
Rogier De Nys <rogierdenys@gmail.com>
2021-11-07 11:45:04 +0100

    fillRect

--- file1.asm
+++ file1.asm
@@ -3,6 +3,10 @@ P386
 MODEL FLAT, C
 ASSUME cs:_TEXT, ds:FLAT, es:FLAT, fs:FLAT, gs:FLAT
 
+VMEMADR EQU 0A0000h	; video memory address
+SCRWIDTH EQU 320	; screen witdth
+SCRHEIGHT EQU 200	; screen height
+
 CODESEG
 
 ; Set the video mode
@@ -40,15 +44,46 @@ PROC terminateProcess
 	ret
 ENDP terminateProcess
 
+; Draw a filled rectangle
+PROC fillRect
+	ARG @@x0: word, @@y0: word, @@w: word, @@h: word, @@col: byte
+	USES eax, ecx, edx, edi
+	
+	; Compute top left corner
+	movzx eax, [@@y0]
+	mov edx, SCRWIDTH
+	mul edx
+	add ax, [@@x0]
+	mov edi, VMEMADR
+	add edi, eax
+	
+	movzx ecx, [@@h]
+	movzx edx, [@@w]
+	mov al, [@@col]
+@@plotline:
+	push ecx
+	mov ecx, edx
+	rep stosb
+	add edi, SCRWIDTH
+	sub edi, edx
+	pop ecx
+	loop @@plotline
+	
+	ret
+ENDP fillRect
+
 PROC main
 	sti
 	cld
 	
 	push ds
-	pop ds
+	pop es
 	
 	call setVideoMode, 13h
 	
+	call fillRect, 60, 60, 60, 60, 5
+	
+	; exit on esc
 	call waitForSpecificKeystroke, 1Bh
 	call terminateProcess
 ENDP main	
