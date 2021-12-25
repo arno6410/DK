b63bfd491092a8f47e67a106f64378f534e8eac6
Rogier De Nys <rogierdenys@gmail.com>
2021-12-25 15:24:23 +0100

    downwards platformen werkt volledig

--- main.asm
+++ main.asm
@@ -9,6 +9,8 @@ SCRHEIGHT EQU 200	; screen height
 FRAMESIZE EQU 256	; mario size (16x16)
 KEYCNT EQU 89		; number of keys to track
 SPEED EQU 4			; mario's speed 
+JUMP EQU 8			; initial vertical speed in a jump; total jump height is JUMP*(JUMP-1)/2
+NUMOFPF EQU 4		; number of platforms
 
 CODESEG
 
@@ -39,8 +41,18 @@ STRUC barrel
 	h 				dd 0		; height
 	color 			dd 0		; color
 ENDS barrel
+
 PROC drawPlatforms
+	USES eax, ecx
+
+	; ecx = number of platforms
+	mov ecx, NUMOFPF
+@@drawPlatformLoop:
+	mov eax, [platformList + 4*ecx-4]
+	call platform_both, [eax + newPlatform.x0], [eax + newPlatform.y0], [eax + newPlatform.x1], [eax + newPlatform.y1], [eax + newPlatform.h], [eax + newPlatform.color]
+	loop @@drawPlatformLoop
 	
+	ret
 ENDP drawPlatforms
 
 PROC scrollUp
@@ -61,15 +73,10 @@ PROC checkCharCollision
 	mov ecx, [@@o_pf]
 	
 	; check if the char is under the platform
-	; only if speed_y < 0
-;	cmp [ebx + character.speed_y], 0
-;	jg @@speedYpos
 	mov eax, [ecx + newPlatform.y0]
 	add eax, [ecx + newPlatform.h]
 	cmp eax, [ebx + character.y]
 	jl @@nocol
-	
-@@speedYpos:
 	
 	mov eax, [ebx + character.x]
 	cmp eax, [ecx + newPlatform.x1]
@@ -79,9 +86,9 @@ PROC checkCharCollision
 	jge @@check
 	
 @@noXoverlap:
+	mov [ebx + character.in_the_air], -1
 	cmp [ebx + character.currentPlatform], ecx
 	je @@nocol
-	mov [ebx + character.in_the_air], -1
 	ret
 	
 	; check for collision	
@@ -91,18 +98,16 @@ PROC checkCharCollision
 	cmp eax, -1
 	je @@in_the_air
 	
-	; only if speed_y > 0
-	cmp [ebx + character.speed_y], 0
-	jl @@in_the_air
-	
 	; collision!
 	mov [ebx + character.speed_y], 0
 	mov [ebx + character.in_the_air], 0
 	mov [ebx + character.y], eax
 	mov [ebx + character.currentPlatform], ecx
-	jmp @@nocol
+	ret
+	
 @@in_the_air:
 	; om te checken of mario echt in de lucht is, kijken we of er collision zou zijn als we mario enkele pixels naar beneden zouden verschuiven
+	mov [ebx + character.in_the_air], 0
 	mov eax, [ebx + character.y]
 	add eax, 2
 	call collision_down, [ebx + character.x], eax, [ebx + character.w], [ebx + character.h], \
@@ -188,7 +193,6 @@ ENDP checkCollision
 ; only to be used if pf is not the char's currentPlatform
 PROC x_collision
 	ARG @@o_char: dword, @@o_pf: dword
-;	LOCAL
 	USES eax, ebx, ecx
 	
 	mov ebx, [@@o_char]
@@ -198,24 +202,18 @@ PROC x_collision
 	je @@nocol
 	
 	; check if the char is under the platform
-	; only if speed_y < 0
-;	cmp [ebx + character.speed_y], 0
-;	jg @@speedYpos
 	mov eax, [ecx + newPlatform.y0]
 	add eax, [ecx + newPlatform.h]
 	cmp eax, [ebx + character.y]
 	jl @@nocol
-	
-@@speedYpos:
 
-	
 	mov eax, [ebx + character.x]
 	cmp eax, [ecx + newPlatform.x1]
 	jg @@nocol
 	
 	add eax, [ebx + character.w]
 	cmp eax, [ecx + newPlatform.x0]
-	jle @@nocol
+	jl @@nocol
 	
 	call collision_down, [ebx + character.x], [ebx + character.y], [ebx + character.w], [ebx + character.h], \
 		[ecx + newPlatform.x0], [ecx + newPlatform.y0], [ecx + newPlatform.x1], [ecx + newPlatform.y1]
@@ -233,7 +231,6 @@ PROC x_collision
 	mov eax, [ecx + newPlatform.x1]
 	inc eax
 	mov [ebx + character.x], eax
-	mov [ebx + character.color], 1h
 	ret
 	
 @@rightwards:
@@ -320,23 +317,18 @@ checkEsc:
 newgame:
 	; (re-)initialise mario
 	; mario character <40,60,0,0,16,20,33h,0,0,0>
-	mov [mario.x], 40
+	mov [mario.x], 100
 	mov [mario.y], 60
 	mov [mario.speed_x], 0
 	mov [mario.speed_y], 0
 	mov [mario.w], 16
 	mov [mario.h], 20
 	mov [mario.in_the_air], -1
-	mov [mario.currentPlatform], offset ground1
+	mov [mario.currentPlatform], offset ground2
 	
 	call fillRect, 0, 0, 320, 200, 0h
 	
-	; ecx = number of platforms
-	mov ecx, 4
-drawPlatformLoop:
-	mov eax, [platformList + 4*ecx - 4]
-	call platform_both, [eax + newPlatform.x0], [eax + newPlatform.y0], [eax + newPlatform.x1], [eax + newPlatform.y1], [eax + newPlatform.h], [eax + newPlatform.color]
-	loop drawPlatformLoop
+	call drawPlatforms
 	
 mainloop:
 	
@@ -387,7 +379,7 @@ noRight:
 	mov ebx, [offset __keyb_keyboardState + 11h] ;Z
 	cmp ebx, 1
 	jne noUp	
-	mov [mario.speed_y], -8
+	mov [mario.speed_y], -JUMP
 	mov [mario.in_the_air], -1
 	
 noUp:
@@ -412,12 +404,16 @@ noUp:
 skippp:
 	call fillRect, eax, ebx, [mario.w], [mario.h], [mario.color]
 nxt:
+push eax
+	call drawPlatforms
+	mov eax, [mario.currentPlatform]
+;	call platform_both, [eax + newPlatform.x0], [eax + newPlatform.y0], [eax + newPlatform.x1], [eax + newPlatform.y1], [eax + newPlatform.h], 43h
 	
 	call wait_VBLANK, 3
 	
+pop eax
 	; undraw mario
 	call fillRect, eax, ebx, [mario.w], [mario.h], 0h	
-	
 	
 noJump:
 	; gravity
@@ -425,22 +421,22 @@ noJump:
 	
 ; check for collision
 	; if mario is in the air, currentPlatform can change
-	mov ecx, 4
+	mov ecx, NUMOFPF
 	cmp [mario.in_the_air], -1
 	je checkAllGrounds
 	
 	call checkCharCollision, offset mario, [mario.currentPlatform]
-;	call x_collision, offset mario, offset ground1
-;	call x_collision, offset mario, offset testground
-	
 checkX_collisionLoop:
-	call x_collision, offset mario, [platformList + 4*ecx - 4]
+	call x_collision, offset mario, [platformList + 4*ecx-4]
 	loop checkX_collisionLoop
-	
 	jmp rest
 	
 checkAllGrounds:
-	call checkCharCollision, offset mario, [platformList + 4*ecx - 4]
+	mov [mario.currentPlatform], 0
+	call checkCharCollision, offset mario, [platformList + 4*ecx-4]
+	mov eax, [mario.currentPlatform]
+	cmp eax, [platformList + 4*ecx - 4]
+	je rest
 	loop checkAllGrounds
 	
 rest:
@@ -462,11 +458,10 @@ ENDP main
 
 DATASEG
 	mario character <40,60,0,0,16,20,33h,1>
-	testground newPlatform <150,162,190,167,10,25h>
-	ground1 newPlatform <15,180,295,185,10,25h>
-	ground2 newPlatform <240,160,280,162,5,25h>
-	ground3 newPlatform <180,140,220,142,5,25h>
-	ground4 newPlatform <120,115,150,118,5,25h>
+	ground1 newPlatform <20,180,60,185,10,25h>
+	ground2 newPlatform <80,160,120,162,10,25h>
+	ground3 newPlatform <150,140,190,142,10,25h>
+	ground4 newPlatform <240,120,290,118,10,25h>
 	platformList dd ground1,ground2,ground3,ground4
 
 	openErrorMsg db "could not open file", 13, 10, '$'
@@ -482,12 +477,6 @@ DATASEG
 	msgControlsUp		db "Z: UP/JUMP", 13, 10, '$'
 	msgControlsDown		db "S: DOWN", 13, 10, '$'
 	msgControlsEnter	db "ENTER: SELECT", 13, 10, '$'
-	
-;	keybscancodes 	db 29h, 02h, 03h, 04h, 05h, 06h, 07h, 08h, 09h, 0Ah, 0Bh, 0Ch, 0Dh, 0Eh, 	52h, 47h, 49h, 	45h, 35h, 00h, 4Ah
-;					db 0Fh, 10h, 11h, 12h, 13h, 14h, 15h, 16h, 17h, 18h, 19h, 1Ah, 1Bh, 		53h, 4Fh, 51h, 	47h, 48h, 49h, 		1Ch, 4Eh
-;					db 3Ah, 1Eh, 1Fh, 20h, 21h, 22h, 23h, 24h, 25h, 26h, 27h, 28h, 2Bh,    						4Bh, 4Ch, 4Dh
-;					db 2Ah, 00h, 2Ch, 2Dh, 2Eh, 2Fh, 30h, 31h, 32h, 33h, 34h, 35h, 36h,  			 48h, 		4Fh, 50h, 51h,  1Ch
-;					db 1Dh, 0h, 38h,  				39h,  				0h, 0h, 0h, 1Dh,  		4Bh, 50h, 4Dh,  52h, 53h
 
 UDATASEG;
 	;filehandle dw ?
